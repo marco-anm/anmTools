33fed50d87c5dc2ae33bce997f64ab94140a6ab0
marco-anm <141737814+marco-anm@users.noreply.github.com>
2025-07-25 08:57:33 +0200

    Update and rename xRay.py to Objects/xRay.py

--- Objects/resetValues.py
+++ Objects/resetValues.py
@@ -2,7 +2,7 @@ from maya import cmds
 sel=cmds.ls(sl=True)
 for each in sel:
 	attr = cmds.listAttr(each, k=True)
-	attr.extend(x for x in cmds.listAttr(each, ud=True) if x not in attr)
+	attr.extend(x for x in cmds.listAttr(each, ud=True) if x not in attr) if cmds.listAttr(each, ud=True) else attr
 	for eachAttr in attr:
 		defVal = cmds.attributeQuery(eachAttr, node=each, listDefault=True)
 		try:
